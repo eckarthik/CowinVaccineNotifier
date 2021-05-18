55370ed838bd05a279095ca9f1da2e75848ce8c6
Karthik E C <eckarthik39@gmail.com>
2021-05-18 13:35:58 +0530

    Corrected the page title and Navbar brand

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
         <>
             <Navbar bg="light">
-                <Navbar.Brand><i className="fas fa-syringe" style={{color:"#2e86de",fontSize:"20px"}}></i><span style={{color:"#2e86de"}}>Cowin Slot Notifier</span></Navbar.Brand>
+                <Navbar.Brand><i className="fas fa-syringe" style={{color:"#2e86de",fontSize:"20px"}}></i><span style={{color:"#2e86de"}}>Cowin Shot Notifier</span></Navbar.Brand>
             </Navbar>
             <br />
         </>
